427e5945670a0597bf6e9ad18137c149764a28be
Hamilton <>
2022-12-11 16:06:39 +0100

    remove scrollto,  compress hero.jpg for mediaquery

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-  //Navigation menu scrollTo
-  $("header nav ul li a").click(function (event) {
-    event.preventDefault();
-    var section = $(this).attr("href");
-    var section_pos = $(section).position();
-    if (section_pos) {
-      $(window).scrollTo({ top: section_pos.top, left: "0px" }, 1000);
-    }
-  });
-
-  $(".app_link").click(function (e) {
-    event.preventDefault();
-    $(window).scrollTo({ top: $("#hero").position().top, left: "0px" }, 1000);
-  });
 
   //Show & Hide menu on mobile
   $(".burger_icon").click(function () {
