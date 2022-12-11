d2e84cf5855bf0ec3382e7903cd709e5041422e5
Hamilton <>
2022-12-11 15:08:31 +0100

    defer jquery, compress hero.jpg

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   });
 
   $(".app_link").click(function (e) {
-    e.preventDefault();
+    event.preventDefault();
     $(window).scrollTo({ top: $("#hero").position().top, left: "0px" }, 1000);
   });
 
